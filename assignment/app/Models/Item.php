f4fe7ccb9a5ac0722332b383c16f1a5d78dfe92b
Viraj Abeywardana <abeywardana21@gmail.com>
2024-03-15 15:22:52 +0530

    add item table to fillable

--- assignment/app/Models/Item.php
+++ assignment/app/Models/Item.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'item_name',
+        'description',
+        'manufacturer',
+        'unit_price',
+        'quantity_in_stock',
+    ];
 }
