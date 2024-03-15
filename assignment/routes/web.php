b8587c457f818ad165a037a3f1db9ff65abfccea
Viraj Abeywardana <abeywardana21@gmail.com>
2024-03-15 12:17:04 +0530

    add routes

--- assignment/routes/web.php
+++ assignment/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function(){
+    Route::get('register', 'register')->name('register');
+    Route::get('login', 'login')->name('login');
+    Route::get('logout', 'logout')->name('logout');
 });
